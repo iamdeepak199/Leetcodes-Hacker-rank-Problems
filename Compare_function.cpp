aea2745b47ba86751589aef6144d42fb3235ad00
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-02-25 20:30:51 +0530

    String Compare Function ( )
    Output:
    1) if 0 means Both Strings are Same ;
    2) if 1 means Both Strings are different But in dictionary order;
    3)if -1 means Both Strings are different But Not in dictionary order;

---
+++ Compare_function.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+#include<string>
+using namespace std;
+int main(){
+    string s1="Deepak";
+    string s2="Deepak";
+   int i= s1.compare(s2);
+    cout<<i<<endl;
+
+}
