bb13c5c362959d338ab67d93d0a3cd30e1e48388
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-02-25 20:38:00 +0530

    String Find ( )
    It will gives you index where your string match found;
    
    18

---
+++ Find_function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+#include<string>
+using namespace std;
+int main(){
+    string s1="My Name is Deepak Hello";
+   int i= s1.find("Hello");
+    cout<<i<<endl;
+
+}
