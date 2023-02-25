6d3065d552d78528df13927074cb31667f5dbbca
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-02-25 20:47:54 +0530

    Sting append( )
    input:
    Hello My name is deepak;
    output:
    Hello My name is deepak Friends;

---
+++ append_function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+#include<string>
+using namespace std;
+int main(){
+    string s1="Hello My name is deepak ";
+    s1.append("Friends");
+    cout<<s1<<endl;
+
+}
