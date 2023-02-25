fd39ff2e44572c604e1d9bedece26d0bab498e4a
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-02-25 20:45:12 +0530

    Sting replace ( )
    H*o My name is deepak

---
+++ replace_functions.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+#include<string>
+using namespace std;
+int main(){
+    string s1="Hello My name is deepak ";
+    s1.replace(1,3,"*");
+    cout<<s1<<endl;
+
+}
