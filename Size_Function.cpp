ee82b5d71ad6ecba681fac081c573cca0fa19af3
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-02-25 20:21:48 +0530

    String using Size( )
    String using Size is :31

---
+++ Size_Function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+#include<string>
+using namespace std;
+int main(){
+    string s1="Hell Students my Name Is Deepak";
+    int i =s1.size();
+    cout<<i<<endl;
+
+}
