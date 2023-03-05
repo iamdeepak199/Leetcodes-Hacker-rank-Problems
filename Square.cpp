4477554348d2c08fce49bf671915496f559c2d45
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-03-05 22:02:48 +0530

    Square of Array( )
    Square of Array( )
    square[0] 0
    square[1] 1
    square[2] 4
    square[3] 9
    square[4] 16
    square[5] 25
    square[6] 36
    square[7] 49
    square[8] 64
    square[9] 81
    square[10] 100

---
+++ Square.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int square[11],i;
+	for(int i=0;i<11;i++){
+		square [i]=i*i;
+		cout<<"\n square"<<"["<< i <<"]"<<" "<<square[i];
+			}
+}
