5f233edd5c759b21e7d46a54c8b6dceb9e5e73b1
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-03-10 22:26:28 +0530

    Delete Array elements
    Enter the Size of the array:
    4
    Enter the elements of the array:
    41
    42
    43
    44
    
    45
    Enterd array is :
    41
    42
    43
    44
    45
    Enter which position want to delet in the array:
    2
    Enter the Number which you want to delet in the array:
    42
    Enterd array is :
    41
    43
    44
    45

---
+++ Delete_Array.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int A[50],num,size,pos;
+    cout<<"Enter the Size of the array:"<<endl;
+   cin>>size;
+   cout<<"Enter the elements of the array:"<<endl;
+   for(int i=0;i<=size;i++){
+   cin>>A[i];
+   }
+    cout<<"Enterd array is :"<<endl;
+   for(int i=0;i<=size;i++){
+    cout<<A[i]<<endl;
+   }
+   cout<<"Enter which position want to delet in the array:"<<endl;
+   cin>>pos;
+   cout<<"Enter the Number which you want to delet in the array:"<<endl;
+   cin>>num;
+for(int i=pos-1;i<size;i++){
+    A[i]=A[i+1];
+}
+size--;
+cout<<"Enterd array is :"<<endl;
+   for(int i=0;i<=size;i++){
+    cout<<A[i]<<endl;
+   }
+}
+
